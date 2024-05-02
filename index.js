49b8f7525c37c75c641dce02222693be9cda9281
Thiago <111020021+thiagoconsult@users.noreply.github.com>
2024-05-02 15:54:29 +0000

    Alteração HTTPS

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import express from "express";
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+const PORT = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(3001, () => {
-  console.log("Server rodando na porta 3001");
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
+};
+
+const server = https.createServer(options, app);
+
+server.listen(PORT, () => {
+  console.log(`Server started at port #${PORT}`);
 });
