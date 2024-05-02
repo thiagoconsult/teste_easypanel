ee6e64ff0af189a5286836b7ea02c8ef124cde47
Thiago <111020021+thiagoconsult@users.noreply.github.com>
2024-05-02 16:15:36 +0000

    teste 4

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const express = require("express");
-// const https = require("https");
-// const fs = require("fs");
-// const path = require("path");
+var express = require("express");
+var app = express();
 
-const app = express();
-const PORT = 3001;
+const path = require("path");
 
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({ mesage: "Hello" });
+app.get("/", function (req, res) {
+  res.send("Hello");
 });
 
-app.listen(PORT, () => {
-  console.log("Server rodando na porta 3001");
+app.listen(process.env.PORT ?? 3000, function () {
+  console.log(`Listening on port ${process.env.PORT ?? 3000}...`);
 });
-// const options = {
-//   key: fs.readFileSync(path.join(__dirname, "key.pem")),
-//   cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
-// };
-
-// const server = https.createServer(options, app);
-
-// server.listen(PORT, () => {
-//   console.log(`Server started at port #${PORT}`);
-// });
